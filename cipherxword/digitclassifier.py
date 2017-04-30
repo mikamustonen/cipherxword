a6204d22345a1bceec3e1bec081876691db45367
Mika Mustonen <mika.t.mustonen@gmail.com>
2017-04-30 10:56:21 -0400

    Implemented DigitClassifier using pixel values as features.

--- cipherxword/digitclassifier.py
+++ cipherxword/digitclassifier.py
@@ -3,31 +3,62 @@
 from cipherxword import dataset
 import numpy as np
 import cv2
+from sklearn import svm, metrics
 
 
 class DigitClassifier(object):
     """A classifier for identifying printed digits.
     """
     
-    def __init__(self, validation_set_size=30):
+    def __init__(self, validation_set_size=30, verbose=False):
         # When creating the classifier, train it. Training a support vector
         # machine is so fast it makes no sense to store the fitted model on
         # the disk.
         digit_images, target = dataset.load_digits()
         features = features_pixels(digit_images)
+        self.train(features, target, validation_set_size, verbose)
 
     
-    def train(self, features):
-        raise NotImplementedError()
-    
-    
-    def predict(self):
-        raise NotImplementedError()
-
-
-    def extract_features_pixels(self, images):
+    def train(self, features, target, validation_set_size, verbose):
+        """Trains the classifier. Automatically called when creating the object.
+        
+        Args:
+            features:             the list of samples
+            target:               the labels for the samples
+            validation_set_size:  the number of samples to use for validation
+            verbose:              if True, print the validation report
         """
+        # Split the set into the training and validation set
+        training_set = features[:-validation_set_size]
+        training_target = target[:-validation_set_size]
+        validation_set = features[-validation_set_size:]
+        expected = target[-validation_set_size:]
+        
+        # Train a support vector machine
+        self.classifier = svm.SVC(kernel='linear', gamma=0.01)
+        self.classifier.fit(training_set, training_target)
+        
+        # Optionally print out the validation results
+        if verbose:
+            print("Digit classifier trained using {} samples.".format(
+                len(training_set)))
+            predicted = self.classifier.predict(validation_set)
+            print("\nDigit classifier validation report:")
+            print(metrics.classification_report(expected, predicted))
+            print("\nConfusion matrix:")
+            print(metrics.confusion_matrix(expected, predicted))
+    
+    
+    def predict(self, images):
+        """Identifies the digits in given grayscale images.
+        
+        Args:
+            images: a list of images of digits
         """
+        # Extract the features
+        features = features_pixels(images)
+        
+        return self.classifier.predict(features)
 
 
 # Auxiliary functions
@@ -62,7 +93,7 @@ def preprocess_image(image):
 
 
 def features_pixels(images):
-    """Translates grayscale images to 64-value vectors of brightness values,
+    """Translates grayscale images to vectors of brightness values,
     similar to the sklearn MNIST dataset.
     
     Args:
@@ -71,6 +102,6 @@ def features_pixels(images):
     Returns:
         the list of 64-element vectors
     """
-    features = [cv2.resize(preprocess_image(image), (8, 8)).flatten()
+    features = [cv2.resize(preprocess_image(image), (12, 12)).flatten()
         for image in images]
     return features
