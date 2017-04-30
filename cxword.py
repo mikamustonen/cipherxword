523ae84913597a2e4fae5f101eb980d8c3e7d263
Mika Mustonen <mika.t.mustonen@gmail.com>
2017-04-30 17:38:45 -0400

    Exposed some of the digit recognition options to the command line.

--- cxword.py
+++ cxword.py
@@ -19,6 +19,16 @@ parser.add_argument("--puzzle-border", type=str, default=None,
     help="output file for an image with the detected puzzle border drawn")
 parser.add_argument("--digits", type=str, default=None,
     help="output file for an image with the detected digits drawn")
+parser.add_argument("--digit-min-width", type=float, default=0.05,
+    help="minimum width for a digit in cells")
+parser.add_argument("--digit-max-width", type=float, default=0.4,
+    help="maximum width for a digit in cells")
+parser.add_argument("--digit-min-height", type=float, default=0.1,
+    help="minimum height for a digit in cells")
+parser.add_argument("--digit-max-height", type=float, default=0.5,
+    help="maximum height for a digit in cells")
+parser.add_argument("--filled-cell-threshold", type=float, default=0.8,
+    help="threshold for a cell to be considered filled")
 options = parser.parse_args()
 
 # Read and solve the puzzle
@@ -28,7 +38,11 @@ if options.puzzle_border:
     cv2.imwrite(options.puzzle_border, puzzle_with_border)
 
 print("Reading the puzzle from the image...")
-puzzle = cw.read_puzzle(verbose=True)
+puzzle = cw.read_puzzle(verbose=True, digit_min_width=options.digit_min_width,
+                        digit_max_width=options.digit_max_width,
+                        digit_min_height=options.digit_min_height,
+                        digit_max_height=options.digit_max_height,
+                        filled_cell_threshold=options.filled_cell_threshold)
 if options.digits:
     digits_overlaid = cw.overlay(puzzle)
     cv2.imwrite(options.digits, digits_overlaid)
